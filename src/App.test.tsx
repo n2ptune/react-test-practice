30c63dbe9020c2e906192469060ad01cd2be854c
n2ptune <n2ptune@outlook.com>
2020-12-29 18:23:55 +0900

    basic tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { fireEvent, render } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<App />', () => {
+  test('Render base elements', () => {
+    const utils = render(<App />)
+    utils.getByText('0')
+    utils.getByText('Plus')
+    utils.getByText('Minus')
+  })
+
+  test('Should be rendered increased number', () => {
+    const utils = render(<App />)
+    const n = utils.getByText('0')
+    const increase = utils.getByText('Plus')
+    fireEvent.click(increase)
+    expect(n).toHaveTextContent('1')
+  })
+
+  test('Should be rendered decreased number', () => {
+    const utils = render(<App />)
+    const n = utils.getByText('0')
+    const decrease = utils.getByText('Minus')
+    // fireEvent.click(decrease)
+
+    for (let i = 0; i < 50; i++) {
+      fireEvent.click(decrease)
+    }
+
+    expect(n).toHaveTextContent('-50')
+  })
+})
