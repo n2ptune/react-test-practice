89da08c848f9164444686f41e472f6d8a1c1124e
n2ptune <n2ptune@outlook.com>
2020-12-29 21:22:27 +0900

    use screen object instead of return value of render

--- src/__tests__/Counter.spec.tsx
+++ src/__tests__/Counter.spec.tsx
@@ -1,26 +1,26 @@
-import { fireEvent, render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import App from '../App'
 
 describe('<App />', () => {
   test('Render base elements', () => {
-    const utils = render(<App />)
-    utils.getByText('0')
-    utils.getByText('Plus')
-    utils.getByText('Minus')
+    render(<App />)
+    screen.getByText('0')
+    screen.getByText('Plus')
+    screen.getByText('Minus')
   })
 
   test('Should be rendered increased number', () => {
-    const utils = render(<App />)
-    const n = utils.getByText('0')
-    const increase = utils.getByText('Plus')
+    render(<App />)
+    const n = screen.getByText('0')
+    const increase = screen.getByText('Plus')
     fireEvent.click(increase)
     expect(n).toHaveTextContent('1')
   })
 
   test('Should be rendered decreased number', () => {
-    const utils = render(<App />)
-    const n = utils.getByText('0')
-    const decrease = utils.getByText('Minus')
+    render(<App />)
+    const n = screen.getByText('0')
+    const decrease = screen.getByText('Minus')
     for (let i = 0; i < 50; i++) {
       fireEvent.click(decrease)
     }
